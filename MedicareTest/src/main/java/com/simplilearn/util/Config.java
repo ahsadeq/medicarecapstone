142ffd248e66092fb7eb9e64d3021bbca55ff2bf
ahsadeq <86921774+ahsadeq@users.noreply.github.com>
2022-06-04 01:27:00 +0400

    Update Config.java

--- MedicareTest/src/main/java/com/simplilearn/util/Config.java
+++ MedicareTest/src/main/java/com/simplilearn/util/Config.java
@@ -1,7 +1,8 @@
 package com.simplilearn.util;
 
-import java.io.File;
+
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -12,28 +13,67 @@ public class Config {
 	
 	static Properties properties =null;
 
-    static void loadPropertiesIfNotLoadedAlready() throws Throwable {
-        if(properties==null) {
+    static void loadPropertiesIfNotLoadedAlready() {
+    	
+
+        if(properties == null) {
             properties = new Properties();
-
-            String propFilepath = System.getProperty("user.dir") + "/src/test/resources/" + PROJECT_PROPERTY_FILE;
-            File file = new File(propFilepath); 
-            System.out.println(file.exists());
-
-            InputStream ins = new FileInputStream(propFilepath);
-            properties.load(ins);
+            String propertyFilePath = System.getProperty("user.dir") + "/src/test/resources/" + PROJECT_PROPERTY_FILE;
+            try {
+                InputStream inputStream = new FileInputStream(propertyFilePath);
+                properties.load(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
     public static String getProperty(String name) {
-        try {
-			loadPropertiesIfNotLoadedAlready();
-		} catch (Throwable e) {
-	
-			e.printStackTrace();
-		}
+        loadPropertiesIfNotLoadedAlready();
         return properties.getProperty(name);
     }
-	
 
 }
+
+
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+//    	
+//        if(properties==null) {
+//            properties = new Properties();
+//
+//            String propFilepath = System.getProperty("user.dir") + "/src/test/resources/" + PROJECT_PROPERTY_FILE;
+//            File file = new File(propFilepath); 
+//            System.out.println(file.exists());
+//
+//            InputStream ins;
+//			try {
+//				ins = new FileInputStream(propFilepath);
+//			} catch (FileNotFoundException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//            properties.load(ins);
+//        }
+//    }
+//
+//    public static String getProperty(String name) {
+//        try {
+//			loadPropertiesIfNotLoadedAlready();
+//		} catch (Throwable e) {
+//	
+//			e.printStackTrace();
+//		}
+//        return properties.getProperty(name);
+//    }
+//	
+//
+//}
